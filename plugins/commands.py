445b0f9a9506aac80e8ae708db6f708758154b8c
Thomas Shelby <85869916+carlgrimez@users.noreply.github.com>
2021-06-14 16:14:01 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -22,7 +22,7 @@ async def start(client, message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/TroJanzHEX")
+                            "MY DEV 👨‍🔬", url="https://t.me/Millieeleven")
                     ]
                 ]
             ),
@@ -45,7 +45,7 @@ async def help(client, message):
                     ],
                     [
                         InlineKeyboardButton(
-                            "⭕️ SUPPORT ⭕️", url="https://t.me/TroJanzSupport")
+                            "😇 SUPPORT 😇", url="https://t.me/Rahulthrilok")
                     ]
                 ]
             ),
